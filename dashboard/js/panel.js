77ac0e78cb736830ae084198f9c2567c496bb970
LShadowFx <luchibarros2006@gmail.com>
2024-11-22 19:31:07 -0300

    conexion a backend

--- dashboard/js/panel.js
+++ dashboard/js/panel.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Configuración de OAuth2
     const CLIENT_ID = '1095478254049177741';
     const REDIRECT_URI = 'https://lshadowfx.github.io/prxyect-web/dashboard/panel.html';
-    const API_BASE = 'https://discord.com/api/oauth2/authorize';
+    const API_BASE = 'https://discord.com/api/oauth2/authorize';    
     const SCOPE = 'identify guilds';
     const RESPONSE_TYPE = 'token';
 
@@ -22,6 +22,25 @@ document.addEventListener('DOMContentLoaded', () => {
     function getTokenFromUrl() {
         const params = new URLSearchParams(window.location.hash.replace('#', '?'));
         return params.get('access_token');
+    }
+
+    // Funcion para conectar con el backend
+    async function fetchServer() {
+        const token = getTokenFromUrl();
+
+        if (!token) {
+            alert('Token no encontrado. Asegúrate de haber iniciado sesión correctamente.');
+            return;
+        }
+
+            try {
+                const response = await fetch(`https://my-backend-3204uuvj5-lshadowfxs-projects.vercel.app/get-servers?token=${token}`);
+                const servers = await response.json();
+
+                console.log(`Servidores: ${servers}`)
+            } catch(e) {
+                console.log(e)
+            }
     }
 
     // Función para cargar servidores desde el backend
