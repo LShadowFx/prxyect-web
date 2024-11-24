04a870132b0b94e92658c51ca15fd6b782f41340
LShadowFx <luchibarros2006@gmail.com>
2024-11-23 23:48:23 -0300

    update server and user fetch

--- dashboard/js/panel.js
+++ dashboard/js/panel.js
@@ -1,5 +1,4 @@
-document.getElementById('login-discord').addEventListener('click', function() {
-    
+document.getElementById('login-discord').addEventListener('click', function () {
     window.location.href = 'https://lproyect-sv.vercel.app/login';
 });
 
@@ -7,8 +6,28 @@ const urlParms = new URLSearchParams(window.location.search);
 const token = urlParms.get('token');
 
 if (token) {
-    document.getElementById('login-discord').style.display = 'none'
+    document.getElementById('login-discord').style.display = 'none';
 
+    // Fetch para obtener datos del usuario
+    fetch('https://discord.com/api/v10/users/@me', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => response.json())
+    .then(user => {
+        // Mostrar datos del usuario
+        const userInfo = document.getElementById('user-info');
+        document.getElementById('user-avatar').src = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
+        document.getElementById('user-welcome').innerText = `Bienvenido ${user.username}!`;
+        userInfo.style.display = 'block';
+    })
+    .catch(error => {
+        console.error('Error al obtener datos del usuario:', error);
+    });
+
+    // Fetch para obtener servidores del usuario
     fetch('https://discord.com/api/v10/users/@me/guilds', {
         method: 'GET',
         headers: {
@@ -17,27 +36,19 @@ if (token) {
     })
     .then(response => response.json())
     .then(guilds => {
-        // Mostrar los servidores del usuario
+        // Mostrar lista de servidores
         const guildListElement = document.getElementById('guilds-list');
         guilds.forEach(guild => {
-            // Crear un nuevo item de lista para cada servidor
             const listItem = document.createElement('li');
             listItem.classList.add('guild-item');
             listItem.innerText = guild.name;
-
             guildListElement.appendChild(listItem);
         });
+        document.getElementById('server-list').style.display = 'block';
     })
     .catch(error => {
         console.error('Error al obtener los servidores:', error);
     });
-
-
-    console.log("Usuario autenticado con el token: ", token);
 } else {
     document.getElementById('login-discord').style.display = 'block';
-
-    document.getElementById('login-discord').addEventListener('click', function() {
-        window.location.href = 'https://lproyect-sv.vercel.app/login';
-    });
 }
